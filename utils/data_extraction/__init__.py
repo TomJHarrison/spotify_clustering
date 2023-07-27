7abd2f60c752c1b33f7d0448162f1ea74d41929e
Tom Harrison <harrisontom612@gmail.com>
2023-07-28 00:46:56 +0100

    Pull through production label in chunks as spotipy cannot deal with many requests at once

--- utils/data_extraction/__init__.py
+++ utils/data_extraction/__init__.py
@@ -38,9 +38,15 @@ def get_playlist_track_features(
                 track_id_list += [track_dict.get('track').get('id')]
 
             # we cannot access the information for every track at once due to request constraints, therefore
-            # we will do this in smaller chunks
-            album_id_list = [album_dict.get('album').get('id') for album_dict in spotipy_client.tracks(track_id_list).get('tracks')]
-            label_list += [album_dict.get('label') for album_dict in spotipy_client.albums(album_id_list).get('albums')]    
+            # we will do this in 4 smaller chunks
+            num_chunks = 10
+            for ii in range(num_chunks):
+                track_id_sublist = track_id_list[
+                    int(ii * len(track_id_list) / num_chunks): 
+                    int((ii + 1) * len(track_id_list) / num_chunks)
+                ]
+                album_id_list = [album_dict.get('album').get('id') for album_dict in spotipy_client.tracks(track_id_sublist).get('tracks')]
+                label_list += [album_dict.get('label') for album_dict in spotipy_client.albums(album_id_list).get('albums')]    
             
             track_audio_features_list += spotipy_client.audio_features([track_id for track_id in track_id_list if isinstance(track_id, str)])
 
@@ -50,8 +56,16 @@ def get_playlist_track_features(
         for track_dict in playlist_tracks_dict.get('items'):
             track_id_list += [track_dict.get('track').get('id')]
 
-        album_id_list = [album_dict.get('album').get('id') for album_dict in spotipy_client.tracks(track_id_list).get('tracks')]
-        label_list += [album_dict.get('label') for album_dict in spotipy_client.albums(album_id_list).get('albums')]
+        # we cannot access the information for every track at once due to request constraints, therefore
+        # we will do this in 4 smaller chunks
+        num_chunks = 10
+        for ii in range(num_chunks = 10):
+            track_id_sublist = track_id_list[
+                int(ii * len(track_id_list) / num_chunks): 
+                int((ii + 1) * len(track_id_list) / num_chunks)
+            ]
+            album_id_list = [album_dict.get('album').get('id') for album_dict in spotipy_client.tracks(track_id_sublist).get('tracks')]
+            label_list += [album_dict.get('label') for album_dict in spotipy_client.albums(album_id_list).get('albums')]    
             
         track_audio_features_list += spotipy_client.audio_features([track_id for track_id in track_id_list if isinstance(track_id, str)])
     
