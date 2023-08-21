5343ed5a02ef153734a558ea4ce41117df172da3
Tom Harrison <harrisontom612@gmail.com>
2023-08-21 21:27:02 +0100

    Change logic to catch None in list before converting to dataframe

--- utils/data_extraction/__init__.py
+++ utils/data_extraction/__init__.py
@@ -36,10 +36,11 @@ def get_playlist_track_features(
 
             track_id_list = []
             for track_dict in playlist_tracks_dict.get('items'):
-                track_id_list += [track_dict.get('track').get('id')]
+                if track_dict is not None:
+                    track_id_list += [track_dict.get('track').get('id')]
 
             # we cannot access the information for every track at once due to request constraints, therefore
-            # we will do this in 4 smaller chunks
+            # we will do this in `num_chunks` smaller chunks
             num_chunks = 10
             for ii in range(num_chunks):
                 track_id_sublist = track_id_list[
@@ -49,21 +50,22 @@ def get_playlist_track_features(
                 
                 # store track names to check later 
                 # track_name_list += [track_dict.get('name') for track_dict in spotipy_client.tracks(track_id_sublist).get('tracks')]
-                album_id_list = [track_dict.get('album').get('id') for track_dict in spotipy_client.tracks(track_id_sublist).get('tracks')]
-                label_list += [album_dict.get('label') for album_dict in spotipy_client.albums(album_id_list).get('albums')]    
-            
+                album_id_list = [track_dict.get('album').get('id') for track_dict in spotipy_client.tracks([track_id for track_id in track_id_sublist if isinstance(track_id, str)]).get('tracks')]
+                label_list += [album_dict.get('label') if album_dict is not None else None for album_dict in spotipy_client.albums(album_id_list).get('albums')]    
+ 
             track_audio_features_list += spotipy_client.audio_features([track_id for track_id in track_id_list if isinstance(track_id, str)])
-
+    
             counter += 1
     else:
         track_id_list = []
         for track_dict in playlist_tracks_dict.get('items'):
-            track_id_list += [track_dict.get('track').get('id')]
+            if track_dict is not None:
+                track_id_list += [track_dict.get('track').get('id')]
 
         # we cannot access the information for every track at once due to request constraints, therefore
         # we will do this in 4 smaller chunks
         num_chunks = 10
-        for ii in range(num_chunks = 10):
+        for ii in range(num_chunks):
             track_id_sublist = track_id_list[
                 int(ii * len(track_id_list) / num_chunks): 
                 int((ii + 1) * len(track_id_list) / num_chunks)
@@ -72,16 +74,18 @@ def get_playlist_track_features(
             # store track names to check later 
             # track_name_list += [track_dict.get('name') for track_dict in spotipy_client.tracks(track_id_sublist).get('tracks')]
             album_id_list = [album_dict.get('album').get('id') for album_dict in spotipy_client.tracks(track_id_sublist).get('tracks')]
-            label_list += [album_dict.get('label') for album_dict in spotipy_client.albums(album_id_list).get('albums')]    
+            label_list += [album_dict.get('label') for album_dict in spotipy_client.albums(album_id_list).get('albums') if album_dict is not None]    
             
         track_audio_features_list += spotipy_client.audio_features([track_id for track_id in track_id_list if isinstance(track_id, str)])
     
     # the dictionaries for some tracks come through as `None`, so we remove them here to prevent errors when trying to
     # convert to a dataframe
-    track_audio_features_list = [track_audio_features for track_audio_features in track_audio_features_list if track_audio_features is not None]
+    mask = np.where(np.array(track_audio_features_list) != None, True, False)
+    track_audio_features_list = list(compress(track_audio_features_list, mask))
+    label_list = list(compress(label_list, mask))
     
     df_output = pd.DataFrame(track_audio_features_list)
-    df_output['track_name'] = track_name_list
+    
     df_output['label'] = label_list
     
     return df_output
